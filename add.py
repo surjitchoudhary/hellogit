532ac9e32b333f59e4111bcb173e89b1baae90f0
surjit9592476079@gmail.com <SurjitSingh@localhost.localdomain>
2020-08-27 13:48:39 +0530

    trying to add new files on git

---
+++ add.py
@@ -0,0 +1,7 @@
+import os
+import psutil
+pid = os.getpid()
+py = psutil.Process(pid)
+memoryUse = py.memory_info()[0]/2.**30  # memory use in GB...I think
+print('memory use:', memoryUse)
+print('you can use memory in linux operating system')
